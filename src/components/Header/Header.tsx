b9362293eb484f5bd4fdd4c25cddd4a5349a6da7
Ivan D <i.derzhylo@softpositive.com>
2022-09-21 20:31:30 +0300

    feat(homepage): add HomePage structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,6 @@ const Header: React.FC = () => {
     <div className="header">
       <div className="headerWrapper container">
         <p className="logo">Shop</p>
-        <div className="buttons">
-          <div className="buttons__wrapper">
-            <div>
-              <button>grd</button>
-              <button>col</button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,30 @@
+// import columnIcon from "assets/img/columnsIcon.svg";
+import { ReactComponent as ColumnsIcon } from "assets/img/columnsIcon.svg";
+import { ReactComponent as GridIcon } from "assets/img/gridIcon.svg";
 import { AppLayout } from "components";
+import "./HomePage.scss";
 
 const HomePage: React.FC = () => {
-  return <AppLayout>{""}</AppLayout>;
+  return (
+    <AppLayout>
+      <div className="content">
+        <div className="content__controlPanel">
+          <div className="buttonsBox">
+            <button>
+              <ColumnsIcon color="" />
+            </button>
+            <button>
+              <GridIcon color="" />
+            </button>
+          </div>
+        </div>
+        <div className="content__wrapper">
+          <div className="content__sidebar"></div>
+          <div className="content__items"></div>
+        </div>
+      </div>
+    </AppLayout>
+  );
 };
 
 export default HomePage;
